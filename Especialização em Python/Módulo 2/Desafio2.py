ae5ed161e270dc022d1045b6a7d0f8fd3669fbfc
BeemoVrumVrum <gbernardo21@gmail.com>
2025-02-03 18:53:18 -0300

    Alteração no Desafio2.py

--- Especialização em Python/Módulo 2/Desafio2.py
+++ Especialização em Python/Módulo 2/Desafio2.py
@@ -8,3 +8,13 @@ Usando os dados obtidos no módulo 1, exiba as seguintes informações:
 
 Parabéns, houve um sorteio e você ganhou um cartão de compras no valor de (valor sorteado).
 '''
+
+cartoes = ['R$50,00','R$250,00','R$120,00']
+
+nome_usuario = str(input("Digite seu nome:"))
+idade_usuario = int(input("Digite sua idade:"))
+data_registro = datetime.now()
+print("Data de registro:",format(data_registro,"%d/%m/%Y"))
+aniversario_usuario = datetime.strptime(input('Digite a sua data de nascimento no formato (dd/mm/yyyy):'),'%d/%m/%Y')
+cartao_usuario = print("Parabéns! Houve um sorteio e você ganhou um vale-compras no valor de ",random.choice(cartoes))
+print(f"Data de nascimento:{format(aniversario_usuario,'%d/%m/%Y')}")
