bbaae898b78c43548951373e42f42677889f93f4
BeemoVrumVrum <gbernardo21@gmail.com>
2025-02-05 07:21:28 -0300

    Desafio1 módulo 5 adicionado

---
+++ Especialização em Python/Módulo 5/Desafio1.py
@@ -0,0 +1,18 @@
+# Desafio 1 Crie uma lista que tenha os nomes dos 3 objetos que você mais usa durante o dia e imprima ele na tela
+
+objetos = ['pc','vape','celular']
+print(objetos)
+
+# Desafio #2 Usando apenas uma linha de código, crie uma lista de 10 a 131
+
+print(list(range(10,132)))
+
+lista_10_a_131 = list(range(10,132))
+# Desafio #3 Imprima na tela o resultado da combinação da lista do desafio 1 e desafio 2
+
+print(objetos+lista_10_a_131)
+
+# Desafio #4 Crie uma lista de listas(matriz) que tenha os nomes dos 3 objetos
+
+matriz = [['pc',1],['vape',2],['celular',3]]
+print(matriz)
