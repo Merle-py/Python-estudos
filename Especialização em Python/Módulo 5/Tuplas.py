516a7bacdfbebbe5959821cc1dd772de6c920d12
BeemoVrumVrum <gbernardo21@gmail.com>
2025-02-05 08:02:24 -0300

    Adicionado Tuplas ao módulo 5

---
+++ Especialização em Python/Módulo 5/Tuplas.py
@@ -0,0 +1,12 @@
+# Tuplas não podem ser alteradas e são dadas entre ()
+
+valores = (1,2,3,4)
+print(valores)
+
+# Exemplo:
+# valores[2] = 10
+
+# União de tuplas
+valores_2 = (5,6,7,8)
+valores_total = valores + valores_2
+print(valores_total)
