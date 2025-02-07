168497d120157b8943bf1234fbb0a34db0990d4f
BeemoVrumVrum <gbernardo21@gmail.com>
2025-02-06 23:35:05 -0300

    Como trabalhar com arquivos Json e erro de arquivo não encontrado corrigidos

--- Especialização em Python/Módulo 5/Json.py
+++ Especialização em Python/Módulo 5/Json.py
@@ -1,5 +1,15 @@
 import json
 
-with open('arquivos_json_usuarios/usuarios1.json', encoding='utf-8') as arquivo_json:
+with open(r'C:\Users\gbern\OneDrive\Área de Trabalho\teste git\projeto-teste\Especialização em Python\Módulo 5\jsonUsuarios\usuarios1.json', encoding='utf-8') as arquivo_json:
     data = json.load(arquivo_json) # Converter um arquivo json para uma string
-    print('')
+    print(data["nome"])
+
+with open(r'C:\Users\gbern\OneDrive\Área de Trabalho\teste git\projeto-teste\Especialização em Python\Módulo 5\jsonUsuarios\usuarios2.json', encoding='utf-8') as arquivo_json:
+    data = json.load(arquivo_json) 
+    print(data["permissões"][1])
+
+with open(r'C:\Users\gbern\OneDrive\Área de Trabalho\teste git\projeto-teste\Especialização em Python\Módulo 5\jsonUsuarios\usuarios3.json', encoding='utf-8') as arquivo_json:
+    data = json.load(arquivo_json) 
+    print(data["usuários"][0]["permissões"][1])
+    print(data["usuários"][1]["admin"])
+    print(data["usuários"][0]["telefone"])
